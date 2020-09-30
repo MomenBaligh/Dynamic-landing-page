82a2172aabbd747b174a5495e49463cdac140d70
Mo'men Baligh <momenbaligh55@gmail.com>
2020-09-30 05:05:13 +0200

    make nav bar dynamic

--- js/app.js
+++ js/app.js
@@ -3,10 +3,21 @@ const navBar = document.querySelector('#navbar__list')
 const sections = Array.from(document.querySelectorAll('section'));
 const sectionNum = sections.length;
 
-/**
- * Define Global Variables
- * 
-*/
+//Make nav bar dynamic 
+for(let i = 1; i <= sectionNum; i++) {
+    //Creating new list items in the nav bar
+    const listItem = document.createElement('li');
+    navBar.appendChild(listItem);
+    //Creating new links in each list item
+    const listItemLink = document.createElement('a');
+    listItem.appendChild(listItemLink);
+    //Make the link scroll to the selected section
+    const aTagAtt = document.createAttribute("href");
+    aTagAtt.value = "#section" + i;
+    listItemLink.setAttributeNode(aTagAtt);
+    //Puting the section name in the list item
+    listItemLink.textContent = sections[i-1].getAttribute('data-nav');
+}
 
 
 /**
